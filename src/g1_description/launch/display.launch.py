6bac4cd035616526d18940d0e626c22cd926071f
GAI-313 <nakatogawagai@gmail.com>
2025-11-20 14:27:32 +0900

    Add g1_urdf

---
+++ src/g1_description/launch/display.launch.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+from ament_index_python.packages import get_package_share_directory
+import os
+
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    robot_description = LaunchConfiguration('robot_description')
+    have_wheel = LaunchConfiguration('have_wheel')
+
+    # default_values
+    default_rviz_path = os.path.join(get_package_share_directory('g1_description'), 'rviz', 'g1.rviz')
+
+    # args
+    declare_have_wheel = DeclareLaunchArgument(
+        'have_wheel', default_value='true',
+        description='If Go2W, set True, else, set False'
+    )
+    declare_robot_description = DeclareLaunchArgument(
+        'robot_description',
+        default_value = os.path.join(
+            get_package_share_directory('g1_description'),
+            'urdf', 'g1_comp.urdf'
+        ),
+        description='Full path for robot description.'
+    )
+    ld.add_action(declare_have_wheel)
+    ld.add_action(declare_robot_description)
+
+    # XACRO -> URDF
+    robot_description_urdf =  ParameterValue(
+        Command([
+            'xacro ', robot_description, ' ',
+            'have_wheel:=', have_wheel
+        ]),
+        value_type=str
+    )
+
+    # Nodes
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': robot_description_urdf}]
+    )
+    joint_state_publisher = Node(
+        package='erasers_g1_common_cpp',
+        executable='joint_state_publisher',
+        parameters=[{'have_wheel': have_wheel}]
+    )
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', default_rviz_path]
+    )
+    ld.add_action(robot_state_publisher)
+    ld.add_action(joint_state_publisher)
+    ld.add_action(rviz)
+
+    return ld
